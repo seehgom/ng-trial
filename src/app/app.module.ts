2e2214337af69413028ac51997daacb3549e4a4b
moghees <the.seehgom@gmail.com>
2020-08-09 13:38:32 -0400

    updating angular cli and core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipiesService } from './recipies/recipies.service';
 import { Route, RouterModule } from '@angular/router';
+import { HighLightDirective } from './directives/high-light.directive';
 
 const routes: Route[] = [
   {
